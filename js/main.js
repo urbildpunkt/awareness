120d8d2e2ba80e43730b02e59f8cfe9d0f27aef5
John Quealy <john@volary.org>
2014-08-18 09:35:34 -0700

    First look

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$('.login').hide();
 
+$(document).keyup(function(e) {
+  if(e.which == 27) {
+    // enter pressed
+    $('.login').slideToggle( 'slow' );
+  }
+});
+
+$( ".login .close" ).on( "click", function() {
+  $('.login').slideToggle( 'slow' );
+});
