0a048510a2f471ccddf23722e0c6d6de29289c4a
John Quealy <john@volary.org>
2014-08-19 10:47:50 -0700

    Editing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
-$('.login').hide();
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 $(document).keyup(function(e) {
   if(e.which == 27) {
     // enter pressed
-    $('.login').slideToggle( 'slow' );
+    $('.login').toggleClass('open');
   }
 });
 
 $('.login .close').on('click', function() {
-  $('.login').slideToggle('slow');
+  $('.login').toggleClass('open');
 });
+
+$('.edit').on('click', function() {
+  $(this).find('i').removeClass('fa-pencil');
+  $(this).find('i').addClass('fa-circle-o-notch');
+  $(this).find('i').addClass('fa-spin');
+  $(this).find('span').text('');
+});
+
+var stream = $('.stream');
+var position = stream.position();
+var fixed = position.top - 50;
+var set = 'translateY(-' + fixed + 'px)';
+$('.stream .edit').on('click', function() {
+  $('.stream').css({'-webkit-transform': set});
+  $('#navbar-stream').hide();
+  $('#navbar-stream-edit').show();
+});
+
+$('.save').on('click', function() {
+  $(this).find('i').removeClass('fa-check');
+  $(this).find('i').addClass('fa-circle-o-notch');
+  $(this).find('i').addClass('fa-spin');
+  $(this).find('span').text('');
+});
+
 
 $('.stream-content li').on('click', function() {
   $('.stream-content li').not(this).removeClass('col-md-6');
